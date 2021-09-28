5a41d78a6868cd8c39a0d9c98648d3636db5b4ac
nassie <nasirah1192@gmail.com>
2021-09-28 23:51:05 +0530

    room dao

--- app/src/main/java/com/example/testspacex/db/LocalDatabase.kt
+++ app/src/main/java/com/example/testspacex/db/LocalDatabase.kt
@@ -1,8 +1,12 @@
 package com.example.testspacex.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.testspacex.model.SpaceXModelItem
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
 
 @Database(
     version = 1, entities = [SpaceXModelItem::class],
@@ -10,4 +14,25 @@ import com.example.testspacex.model.SpaceXModelItem
 )
 abstract class LocalDatabase():RoomDatabase() {
 
+    abstract fun spaceXDao():SpaceXDao
+
+    companion object{
+        var localDatabaseInstance:LocalDatabase? = null
+
+        @InternalCoroutinesApi
+        @Synchronized
+        fun getInstance(context: Context):LocalDatabase{
+            if (localDatabaseInstance == null){
+                synchronized(LocalDatabase::class.java){
+                    localDatabaseInstance = Room.databaseBuilder(
+                        context,LocalDatabase::class.java,"spacedb"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return localDatabaseInstance as LocalDatabase
+        }
+    }
 }
